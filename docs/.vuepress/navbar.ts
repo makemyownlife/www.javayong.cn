3e2e2583619845ad780c0421bb39736ca1039ffc
makemyownlife <zhangyong7120180@163.com>
2023-11-14 12:59:39 +0800

    112

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -3,8 +3,8 @@ import {navbar} from "vuepress-theme-hope";
 export default navbar([
     //{ text: "Java基础", icon: "java",  link: "/home.md" },
     {text: "缓存", icon: "redis", link: "/cache/"},
-    {text: "分库分表", icon: "database", link: "/sharding/"},
     {text: "消息队列", icon: "MQ", link: "/mq/"},
+    {text: "分库分表", icon: "database", link: "/sharding/"},
     {
         text: "程序人生", icon: "article", link: "/high-quality-technical-articles/",
     },
