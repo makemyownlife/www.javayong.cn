595fcbf4f0f46f9d7e2d9dfaa40b450b562528fe
makemyownlife <zhangyong7120180@163.com>
2023-11-16 21:40:28 +0800

    导航栏

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -4,7 +4,7 @@ export default navbar([
     //{ text: "Java基础", icon: "java",  link: "/home.md" },
     {text: "缓存实战", icon: "redis", link: "/cache/01fourJDKlocalcache.html"},
     {text: "消息队列", icon: "MQ", link: "/mq/rocketmq4/00RocketMQ4_introduce.html"},
-    {text: "分库分表", icon: "database", link: "/sharding/"},
+   // {text: "分库分表", icon: "database", link: "/sharding/"},
     {
         text: "程序人生", icon: "article", link: "/high-quality-technical-articles/",
     },
