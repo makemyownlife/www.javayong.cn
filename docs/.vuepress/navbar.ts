1d3aa037d7d06bc535574aa57976ff45f96e5f48
makemyownlife <zhangyong7120180@163.com>
2023-11-20 14:32:00 +0800

    导航栏

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -6,7 +6,7 @@ export default navbar([
     {text: "消息队列", icon: "Kafka", link: "/mq/rocketmq4/00RocketMQ4_introduce.html"},
    // {text: "分库分表", icon: "database", link: "/sharding/"},
     {
-        text: "程序人生", icon: "life-ring", link: "/codelife/",
+        text: "程序人生", icon: "life-ring", link: "/codelife/runningforcode.html",
     },
     {
         text: "B站视频",
