6f860ca2163121c8bb26e022b0a395eaa5ebd559
zhangyong <zhangyong7120180@163.com>
2023-12-21 00:07:51 +0800

    index.tx

--- docs/.vuepress/sidebar/index.ts
+++ docs/.vuepress/sidebar/index.ts
@@ -12,55 +12,73 @@ export default sidebar({
                 "00localandclustercache",
                 "01fourJDKlocalcache",
                 "02pagelistcache",
+                "13GuavaCache.md",
                 "05boolfilter",
                 "07Redistransaction",
                 "09SpringCache",
                 "10DruidDataSource.md",
                 "11CommonsPool2.md",
-                "12howtodesigncachesdk.md",
-                "13GuavaCache.md"
+                "12howtodesigncachesdk.md"
             ]
         },
         {
             text: "消息队列",
-            icon: "Kafka",
-            prefix: "mq",
-            collapsible: true,
-            children: [
-                {
-                    text: "RocketMQ4.X设计精要",
-                    prefix: "rocketmq4/",
-                    collapsible: true,
-                    icon: "apacherocketmq",
-                    children: [
-                        "00RocketMQ4_introduce",
-                        "01RocketMQ4_artch",
-                        "01RocketMQ4_network",
-                        "02RocketMQ4_nameserver",
-                        "03RocketMQ4_producer",
-                        "04RocketMQ4_store",
-                        "06RocketMQ4_consumer",
-                        "07RocketMQ4_broadcast_consumer",
-                        "08RocketMQ4_masterslave",
-                        "10RocketMQ4_transaction",
-                        "11RocketMQ4_messagetrack",
-                        "13RocketMQ4_subscribe_consistent",
-                        "14RocketMQ4_mideng"
-                    ],
-                }
-            ]
-        },
-        {
-            text: "技术人生",
-            icon: "life-ring",
-            prefix: "codelife",
-            collapsible: true,
-            children: [
-                "runningforcode.md",
-                "messagequeuecareer.md",
-                "howtolearnopenproject.md",
-                "guotuxuexistorm.md",
-            ]
-        }
+            icon
+:
+"Kafka",
+    prefix
+:
+"mq",
+    collapsible
+:
+true,
+    children
+:
+[
+    {
+        text: "RocketMQ4.X设计精要",
+        prefix: "rocketmq4/",
+        collapsible: true,
+        icon: "apacherocketmq",
+        children: [
+            "00RocketMQ4_introduce",
+            "01RocketMQ4_artch",
+            "01RocketMQ4_network",
+            "02RocketMQ4_nameserver",
+            "03RocketMQ4_producer",
+            "04RocketMQ4_store",
+            "06RocketMQ4_consumer",
+            "07RocketMQ4_broadcast_consumer",
+            "08RocketMQ4_masterslave",
+            "10RocketMQ4_transaction",
+            "11RocketMQ4_messagetrack",
+            "13RocketMQ4_subscribe_consistent",
+            "14RocketMQ4_mideng"
+        ],
+    }
+]
+}
+,
+{
+    text: "技术人生",
+        icon
+:
+    "life-ring",
+        prefix
+:
+    "codelife",
+        collapsible
+:
+    true,
+        children
+:
+    [
+        "runningforcode.md",
+        "messagequeuecareer.md",
+        "howtolearnopenproject.md",
+        "guotuxuexistorm.md",
     ]
-});
+}
+]
+})
+;
